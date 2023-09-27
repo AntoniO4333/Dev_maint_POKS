4ba27c6d3766602c5f126cd5aee68eebf4428d1c
Anton Cheremushkin <anton43r3m@gmail.com>
2023-09-27 14:16:48 +0700

    задание 1 и задание 2 полностью

---
+++ лаб5/задание2/js/buttons.js
@@ -0,0 +1,20 @@
+// // Получаем кнопку по id
+// var button1 = document.getElementById("myButton1");
+// var button2 = document.getElementById("myButton2");
+// var button3 = document.getElementById("myButton3");
+
+// // Добавляем обработчик события "click" на кнопку
+// button1.addEventListener("click", function() {
+//     // Здесь можно добавить код, который будет выполняться при нажатии на кнопку
+//     alert("Кнопка была нажата!");
+// }); 
+
+// button2.addEventListener("click", function() {
+//     // Здесь можно добавить код, который будет выполняться при нажатии на кнопку
+//     alert("Кнопка была нажата!");
+// }); 
+
+// button3.addEventListener("click", function() {
+//     // Здесь можно добавить код, который будет выполняться при нажатии на кнопку
+//     alert("Кнопка была нажата!");
+// }); 
